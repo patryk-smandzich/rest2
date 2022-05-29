72683b66bf91addd1013ef6bd3a7a14c2f1df59d
Patryk Smandzich <36845962+patryk-smandzich@users.noreply.github.com>
2022-05-29 21:30:52 +0200

    Basic test

---
+++ src/test/java/firstTest.java
@@ -0,0 +1,24 @@
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.Test;
+
+public class firstTest {
+
+    @Test
+    public void getBookDetails() {
+
+        RestAssured.baseURI = "https://demoqa.com/BookStore/v1/Books";
+
+        RequestSpecification httpRequest = RestAssured.given();
+
+        Response response = httpRequest.request(Method.GET, "");
+
+        System.out.println("Status: " + response.getStatusLine());
+
+        System.out.println("Response: " + response.prettyPrint());
+
+    }
+
+}
