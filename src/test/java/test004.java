7c9a86a0a4ab20649a219b50e3921ffe6619d00a
Patryk Smandzich <36845962+patryk-smandzich@users.noreply.github.com>
2022-10-09 14:08:22 +0200

    get more complex jsonpath

--- src/test/java/test004.java
+++ src/test/java/test004.java
@@ -26,4 +26,25 @@ public class test004 {
         System.out.println("Right weight!");
     }
 
+    @Test
+    public void dittoNoDamageFrom() {
+
+        RestAssured.baseURI = "https://pokeapi.co/api/v2/";
+        RequestSpecification httpRequest = RestAssured.given();
+        Response response = httpRequest.get("pokemon/ditto");
+
+        String name = getJsonValue(com.jayway.jsonpath.JsonPath.read(response.asString(), "$..type.name").toString());
+        String url = getJsonValue(com.jayway.jsonpath.JsonPath.read(response.asString(), "$..type.url").toString());
+
+        Response response2 = httpRequest.get(url);
+
+        String no_damage_from = getJsonValue(com.jayway.jsonpath.JsonPath.read(response2.asString(), "$..no_damage_from..name").toString());
+
+        System.out.println("Ditto is a " + name + " type pokemon and does not get damage from " + no_damage_from + " type pokemon!");
+    }
+
+    public String getJsonValue(String value) {
+        return value.replace("[\"", "").replace("\"]", "").replaceAll("\\\\", "");
+    }
+
 }
