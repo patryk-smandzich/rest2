13472a0d9736c7a2c983c15003ff27d41b99df53
Patryk Smandzich <36845962+patryk-smandzich@users.noreply.github.com>
2022-12-24 11:55:30 +0100

    auth

---
+++ src/test/java/test008.java
@@ -0,0 +1,19 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Test;
+
+public class test008 {
+
+    @Test
+    public void authBasic() {
+
+        RequestSpecification httpRequest = RestAssured.given().auth().preemptive().basic("postman", "password");
+        Response res = httpRequest.get("https://postman-echo.com/basic-auth");
+        ResponseBody body = res.body();
+        String rbdy = body.asString();
+        System.out.println("Data from the GET API - " + rbdy);
+    }
+
+}
